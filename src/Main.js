8f206a30f43c4b2c1e869ea1460f9010461058ab
minji4252 <alswl7216@naver.com>
2024-04-17 12:20:32 +0900

    docs: 컴포넌트 반복

--- src/Main.js
+++ src/Main.js
@@ -1,56 +1,64 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 
-const initState = {
-  username: "",
-  message: "",
-};
+const initState = [
+  { id: 1, username: "알라딘" },
+  { id: 2, username: "지니" },
+  { id: 3, username: "홍길동" },
+  { id: 4, username: "임꺽정" },
+  { id: 5, username: "미키마우스" },
+];
 
-//input태그가 여러개 있을 때 함수를 하나로 쓰는 방법
 const Main = () => {
-  const [memberInfo, setMemberInfo] = useState(initState);
-  const { username, message } = memberInfo;
+  // 필터 기본 사용법
+  // const numbers = [1,2,3,4,5];
+  // const bigger = numbers.filter(num => num > 3);
+  // console.log(bigger); // 4,5만 나옴
 
-  const onChange = event => {
-    const nextMemberInfo = {
-      ...memberInfo, //기존의 정보 내용을 이 자리에 복사한 뒤
-      [event.target.name]: event.target.value, //원하는 값을 덮어 씌우기
-    };
-    setMemberInfo(nextMemberInfo);
+  // const remove = numbers.filter(num => num != 3);
+  // console.log(remove); // 1,2,4,5 나옴
+
+  // member 목록 상태
+  const [members, setMembers] = useState(initState);
+  // id 상태
+  const [nextId, setNextId] = useState(6);
+  // input 상태
+  const [username, setUsername] = useState("");
+
+  // input 이벤트 핸들러
+  const onChange = e => {
+    setUsername(e.target.value);
   };
 
   const onClick = () => {
-    alert(`${username}: ${message}`);
-    setMemberInfo(initState); //확인 누르면 초기화 되도록
+    //배열의 내장 함수 concat을 사용하여 새로운 항목을 추가한 배열로 만든다.
+    const nextMembers = members.concat({
+      id: nextId,
+      username: username,
+    });
+    setNextId(nextId + 1);
+    setMembers(nextMembers);
+    setUsername("");
   };
 
-  const onKeyPress = event => {
-    if (event.key === "Enter") {
-      onClick();
-    }
+  // remove 이벤트 핸들러
+  const onRemove = id => {
+    //내가 더블 클릭한 요소와 같지 않은 요소만 남겨라 (더블 클릭한것만 없애라)
+    const nextMembers = members.filter(member => member.id !== id);
+    setMembers(nextMembers);
   };
 
   return (
     <div>
-      <h1>이벤트 연습</h1>
-      <input
-        type="text"
-        name="username"
-        placeholder="사용자명"
-        value={username}
-        onChange={onChange}
-      />
-      <br />
-      <input
-        type="text"
-        name="message"
-        placeholder="아무거나 입력해 보세요"
-        value={message}
-        //내용을 변경할 땐 onChange
-        onChange={onChange}
-        onKeyUp={onKeyPress}
-      />
-      <br />
-      <button onClick={onClick}>확인</button>
+      <input onChange={onChange} value={username} />
+      <button onClick={onClick}>사용자 추가</button>
+
+      <ul>
+        {members.map(member => (
+          <li key={member.id} onDoubleClick={() => onRemove(member.id)}>
+            {member.username}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
